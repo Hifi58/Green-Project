e504a262331f037fbdf9d0a981791cd97de9a16c
Hifi58 <gillet.tony@gmail.com>
2021-05-01 16:15:12 +0200

    first commit

---
+++ Greenlanding.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="greenlanding.css" />
+            <title>A définir</title>
+        </head>
+        <body>
+            <header>
+            <div class="container">
+              <div class="btn"><a href="Accueil.php">Le nom du site</a></div>  
+            </div>   
+            <div class="container2">
+              <div class="btn2"><a href="#">Forum</a></a></div>  
+            </div>    
+            <div class="container3">
+              <div class="btn3"><a href="#">Contact</a></a></div>  
+            </div>
+            <div class="container4">
+              <div class="btn4"><a href="#">Application</a></a></div>  
+            </div>             
+            </header>
+            <div class="pulse">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </body>
+    </html>
---
+++ navigation.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="main.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<header>
+    <a href="greenlanding.php" class="logo">Nom du site</a>
+    <div class="menu-toggle"></div>
+    <nav>
+         <ul>
+             <li><a href="#" class="active">Accueil</a></li>
+             <li><a href="#">Ecosystéme</a></li>
+             <li><a href="#">Biome</a></li>
+             <li><a href="#">Faune</a></li>
+             <li><a href="#">Flore</a></li>
+             <li><a href="#">Action</a></li>
+         </ul>   
+    </nav>
+    <div class="clearfix"></div>
+</header>
